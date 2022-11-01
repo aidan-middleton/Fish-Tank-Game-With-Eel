321fe8f9110263dd253f2b6db84da47fd4510b10
Eduardo Mazuera <edmazuera@gmail.com>
2022-11-01 17:49:36 -0500

    set up tank and camera angle

--- project/Scripts/Eel/EelHeadController.gd
+++ project/Scripts/Eel/EelHeadController.gd
@@ -1,44 +1,45 @@
 extends KinematicBody
 
 const move_speed: float = 4.0
-const rotation_speed: float = 1.0
+const rotation_speed: float = 0.01
 
 onready var parent: Spatial = get_parent()
 
+# default look states
+var UP_LOCATION = Vector3(1, 1, 1)
+
 # Keyboard state
-var _i = false
-var _k = false
-var _j = false
-var _l = false
-var _u = false
-var _o = false
+var _up = false
+var _down = false
+var _left = false
+var _right = false
 
 func _input(event):
 	# Receives key input
 	if event is InputEventKey:
 		match event.scancode:
 			KEY_I:
-				_i = event.pressed
+				_up = event.pressed
 			KEY_K:
-				_k = event.pressed
-			KEY_L:
-				_l = event.pressed
+				_down = event.pressed
 			KEY_J:
-				_j = event.pressed
-			KEY_U:
-				_u = event.pressed
-			KEY_O:
-				_o = event.pressed
+				_left = event.pressed
+			KEY_L:
+				_right = event.pressed
 
 func _physics_process(delta: float) -> void:
 	var _velocity = Vector3(get_global_transform().basis.z * move_speed) 
 	translate(-get_global_transform().basis.z * move_speed * delta)
+
 	
-	var _pitch: float = _k as float - _i as float
-	var _yaw: float = _l as float - _j as float
-	var _roll: float = _o as float - _u as float
+	var _pitch: float = _up as float - _down as float
+	var _yaw: float = _left as float - _right as float
 	
-	var _direction = Vector3(_pitch,_yaw,_roll)
+	var _direction = Vector3(_pitch,_yaw,0)
 	rotate(_direction, rotation_speed)
 	
-	
+#	var rot = Quat(rotation)
+#	look_at(UP_LOCATION, Vector3.UP)
+#	var target_rot = Quat(rotation)
+#	rotation = rotation.slerp(target_rot, rotation_speed)
+#	transform.basis = Basis(rotation)
