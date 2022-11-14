ae3ca94f4afbb9844172d805ceddb30950500428
unknown <aidanrmiddleton@gmail.com>
2022-11-14 02:44:05 -0600

    Added walls for snake collision. Messed with snake controller to allow it to collide

--- project/Scripts/Eel/EelBodyController.gd
+++ project/Scripts/Eel/EelBodyController.gd
@@ -8,7 +8,7 @@ var parent: KinematicBody = null
 
 func follow_parent() -> void:
 	self.global_transform.origin = lerp(self.global_transform.origin, 
-		parent.global_transform.origin, get_physics_process_delta_time() * 3.0)
+		parent.global_transform.origin, get_physics_process_delta_time() * 5.0)
 	self.rotation_degrees = lerp(self.rotation_degrees, 
 		parent.rotation_degrees, get_physics_process_delta_time() * 5.0)
 
--- project/Scripts/Eel/EelHeadController.gd
+++ project/Scripts/Eel/EelHeadController.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
-const move_speed: float = 4.0
-const rotation_speed: float = 0.01
+const move_speed: float = 10.0
+const rotation_speed: float = 0.1
 
 onready var parent: Spatial = get_parent()
 
@@ -18,24 +18,25 @@ func _input(event):
 	# Receives key input
 	if event is InputEventKey:
 		match event.scancode:
-			KEY_I:
+			KEY_W:
 				_up = event.pressed
-			KEY_K:
+			KEY_S:
 				_down = event.pressed
-			KEY_J:
+			KEY_A:
 				_left = event.pressed
-			KEY_L:
+			KEY_D:
 				_right = event.pressed
 
 func _physics_process(delta: float) -> void:
-	var _velocity = Vector3(get_global_transform().basis.z * move_speed) 
-	translate(-get_global_transform().basis.z * move_speed * delta)
-
+	var _velocity = get_transform().basis.z * move_speed
+	#translate(-get_global_transform().basis.z * move_speed * delta)
+	
+	move_and_slide(_velocity)
 	
 	var _pitch: float = _up as float - _down as float
 	var _yaw: float = _left as float - _right as float
 	
-	var _direction = Vector3(_pitch,_yaw,0)
+	var _direction = Vector3(0,0,0) + get_transform().basis.x * _pitch + get_transform().basis.y * _yaw
 	rotate(_direction, rotation_speed)
 	
 #	var rot = Quat(rotation)
